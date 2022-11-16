3a8326dba89c203b3a7a8a970e88f248a7d5482e
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-11-15 21:06:49 -0300

    Asignando DataAnnotation a los atributos

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace entityframeworkPlatzi.Models
 {
     public class Categoria
     {
+        [Key]
         public Guid CetegoriaId { get; set; }
+        [Required]
+        [MaxLength(150)]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
 
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,12 @@ namespace entityframeworkPlatzi.Models
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
+        [ForeignKey("CategoriaId")]
         public Guid CategoriaId { get; set; }
-
+        [Required]
+        [MaxLength(200)]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public Prioridad PrioridadTarea { get; set; }
@@ -17,6 +22,9 @@ namespace entityframeworkPlatzi.Models
 
         //Categoria sociada a la Tarea
         public virtual Categoria Categoria { get; set; }
+
+        [NotMapped]
+        public string Resumen { get; set; }
     }
 
     public enum Prioridad
