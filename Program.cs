6724491833a8e1ba81e8707eccc711da820ad1da
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-11-16 17:42:56 -0300

    Base de datos en Memoria

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Base de datos en memoria
 builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
 
+
+// builder.Services.AddCors(options =>
+// {
+// options.AddPolicy("MyAllowedOrigins",
+//     policy =>
+//     {
+//         policy.WithOrigins("https://192.168.1.75:5190") // note the port is included 
+//             .AllowAnyHeader()
+//             .AllowAnyMethod();
+//     });
+// });
+
+
 var app = builder.Build();
+// app.UseCors("MyAllowedOrigins");
 
 app.MapGet("/", () => "Hello World!");
 
